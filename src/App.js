142ba78a791e484d8ef4c2b2a6af2549bf980ed9
papertimesthree <31554102+papertimesthree@users.noreply.github.com>
2021-02-28 18:51:11 -0800

    blog-db sync

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  useParams
+} from "react-router-dom";
 import "./styles.css";
+import { fstore } from "./utils/fb";
 
 export default function App() {
+  return (
+    <BrowserRouter>
+      <h1>WK Blog</h1>
+
+      <hr />
+      <List />
+      <hr />
+
+      <Switch>
+        {/* <Route exact path="/" component={List} /> */}
+        <Route path="/post/:title/:docid" component={ViewPost} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function List() {
+  let [titles, setTitles] = useState([]);
+
+  async function loadTitles() {
+    const snapshot = await fstore
+      .collection("blog-posts")
+      .orderBy("date", "desc")
+      .get();
+    setTitles(snapshot.docs);
+  }
+
+  useEffect(() => {
+    loadTitles();
+  }, []);
+
   let [x, setX] = useState(1);
   return (
     <div className="App">
       <h1>Hello - {x}</h1>
-      <button onClick={() => setX(x + 1)}>+</button>
+      {titles.map((t) => (
+        <div key={t.id}>
+          <Link to={`/post/${t.data().title.replaceAll(" ", "-")}/${t.id}`}>
+            {t.data().title}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function ViewPost() {
+  let params = useParams();
+  let [content, setContent] = useState("loading...");
+
+  async function loadContent() {
+    setContent("Loading...");
+    let doc = await fstore.collection("blog-contents").doc(params.docid).get();
+    setContent(doc.data().content);
+  }
+  useEffect(() => {
+    loadContent();
+  }, [params.docid]);
+  return (
+    <div>
+      <h3>{params.title}</h3>
+      <div>{content}</div>
     </div>
   );
 }
---
+++ src/utils/fb.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXAE-It00BVpsZtwcvxIRIjJcwpFUEK5s",
+  authDomain: "wk-4d041.firebaseapp.com",
+  projectId: "wk-4d041",
+  storageBucket: "wk-4d041.appspot.com",
+  messagingSenderId: "867480058756",
+  appId: "1:867480058756:web:fc65d668bfa358fb19bbd1"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const fstore = app.firestore();
+const fauth = app.auth();
+export { app, fstore, fauth };
